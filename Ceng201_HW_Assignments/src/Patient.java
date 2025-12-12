cc8fcf520b1cc409950c41e66f2208fb9333155a
Alper Enes Ersü <alperenesersu.b@gmail.com>
2025-12-12 22:56:41 +0300

    Add files via upload

---
+++ Ceng201_HW_Assignments/src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    int id;
+    String name;
+    int severity;
+    int age;
+    //Requirements for patients
+
+    public Patient(int id,String name,int severity,int age) {
+        this.id = id;
+        this.name = name;
+        this.severity = severity;
+        this.age = age;
+    }
+    public String toString(){
+        return "ID: " + id + ", İsim: " + name + ", Ciddiyet: " + severity + ", Yaş: " + age;
+    }
+}
